09655798d7d4ac89516cec7d5f1b64a99269aadb
Botbedi <145961842+Botbedi@users.noreply.github.com>
2025-09-26 14:25:59 +0200

    stampa domanda completo

--- src/ApiQuestion.java
+++ src/ApiQuestion.java
@@ -6,5 +6,5 @@ public class ApiQuestion {
     public String   difficulty;
     public String   question;
     public String   correct_answer;
-    public List<String> wrong_answer;
+    public List<String> incorrect_answers;
 }
--- src/main.java
+++ src/main.java
@@ -2,21 +2,38 @@ import com.google.gson.Gson;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
 
 public class main {
     public static void main(String[] args) {
+        ApiResponse apiResponse = creaDomande(5,"easy");
         System.out.println("Chi Vuol essere Maturato?");
-        ApiClient apiClient = new ApiClient();
-        String s = apiClient.fetchQuestion(10,"easy","multiple");
-        Gson gson = new Gson();
-        ApiResponse apiResponse = gson.fromJson(s,ApiResponse.class);
-        String name=inserireNome();
-        PlayerStatics playerStatics = new PlayerStatics(name);
-
-    }
-    private static String inserireNome(){
         Scanner scanner = new Scanner(System.in);
+        String name=inserireNome(scanner);
+        PlayerStatics playerStatics = new PlayerStatics(name);
+        for(int i = 0;i<5;i++){
+            stampaDomanda(apiResponse,i);
+            boolean risposta=ottieniRisposta(scanner,apiResponse);
+        }
+    }
+    private static boolean ottieniRisposta(Scanner scanner,ApiResponse apiResponse) {
+        scanner.nextLine();
+
+        return false;
+    }
+    private static void stampaDomanda(ApiResponse apiResponse, int i) {
+        System.out.println(apiResponse.results.get(i).question);
+        List<String> risposte = new ArrayList<>();
+        risposte.add(apiResponse.results.get(i).correct_answer);
+        risposte.addAll(apiResponse.results.get(i).incorrect_answers);
+        Collections.shuffle(risposte);
+        for(char j = 'A';j<'E';j++){
+            System.out.print(j);
+            String risposta =risposte.removeFirst();
+            System.out.println(" - "+risposta);
+        }
+    }
+    private static String inserireNome(Scanner scanner){
         System.out.println("Inserire nome giocatore");
         return scanner.nextLine();
     }
@@ -28,5 +45,11 @@ public class main {
         }catch(IOException e){
             System.out.println("Errore durante il salvataggio delle statistiche: "+e.getMessage());
         }
+    }
+    private static ApiResponse creaDomande(int n, String difficulty){
+        ApiClient apiClient = new ApiClient();
+        String s = apiClient.fetchQuestion(5,difficulty,"multiple");
+        Gson gson = new Gson();
+        return gson.fromJson(s,ApiResponse.class);
     }
 }
